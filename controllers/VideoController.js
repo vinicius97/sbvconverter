a7bc35a7d27e490f7ab0b206d28dda45410c25ca
vinicius97 <contato@viniciuslana.com>
2018-07-10 02:20:49 -0300

    Iniciar configuração de encode de vídeo
    Foi feita a integração básica com o serviço Zencoder para o
    encode de vídeos baseado no repositório do AWS S3

--- controllers/VideoController.js
+++ controllers/VideoController.js
@@ -1,6 +1,24 @@
 const fs = require('fs')
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+const clientEncoder = require('zencoder')('fecbaa90d94d27af5d319d20165b8447')
+
+const handleProgress = (job) => {
+  clientEncoder.Job.progress(job.id, async (err, data) => {
+    await setTimeout(() => console.log(data), 3000)
+  })
+}
+
+const handleEncode = (filename) => {
+  clientEncoder.Job.create({input: `https://s3-sa-east-1.amazonaws.com/sbtfullstack/${filename}`}, function(err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }else{
+      handleProgress(data)
+    }
+  });
+}
 
 module.exports = {
   handleUploadToS3(){
@@ -13,14 +31,28 @@ module.exports = {
       const params = {
         Body: data,
         Bucket: bucketName,
-        Key: "VideoTeste.mp4",
+        Key: 'VideoTeste.mp4'
       }
 
-      s3.putObject(params, function (err, data) {
+      s3.putObject(params, function (err) {
         if (err) {
           console.log(err)
         } else {
           console.log("Dados enviados com sucesso para sbtfullstack/frontend")
+
+          const aclParams = {
+            ACL: 'public-read',
+            Bucket: bucketName,
+            Key: 'VideoTeste.mp4'
+          }
+
+          s3.putObjectAcl(aclParams, function (err, data) {
+            if (err) {
+              console.log(err)
+            } else {
+              handleEncode(params.Key)
+            }
+          })
         }
       })
     })
