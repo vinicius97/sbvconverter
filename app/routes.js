13f152785e5da0441ea405fbd1696532a2cc1b6b
vinicius97 <contato@viniciuslana.com>
2018-07-16 21:36:21 -0300

    Passar DNS da aplicação frontend via config.json para CORS

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const VideoRoute = require('./routes/VideoRoute')
+const config = require('../config.json')
 
 module.exports = (io) => {
 
   router.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
+    res.setHeader('Access-Control-Allow-Origin', config.webapp_domain)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', true)
